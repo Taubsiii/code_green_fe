c746608606812fe57a1c2486af3489d3e098909a
andreas.rossberg <andreas.rossberg@de.markant.com>
2023-10-13 21:28:36 +0200

    Fixed Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
 @Component({
-  selector: 'app-root',
+  selector: 'green-code',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'code_green_fe';
+
+  options: Leaflet.MapOptions = {
+    layers: getLayers(),
+    zoom: 12,
+    center: new Leaflet.LatLng(43.530147, 16.488932)
+  };
+}
+
+export const getLayers = (): Leaflet.Layer[] => {
+  return [
+    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    } as Leaflet.TileLayerOptions),
+  ] as Leaflet.Layer[];
 }
